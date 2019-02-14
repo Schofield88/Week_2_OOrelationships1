dd59d5bc06eb7a8f0c99c67f62d6e02a7348a817
Luke Phyall <lukeprospect@yahoo.com>
2019-02-14 21:25:37 +0000

    Fully refactors code with polymorphic relationships

--- lib/exercise_4.rb
+++ lib/exercise_4.rb
@@ -1,10 +1,10 @@
 class ScrambledDiary
-  def initialize(:contents)
+  def initialize(contents)
     @contents = contents
   end
 
   def read
-    @contents
+    puts @contents
   end
 
   def rejigger(crypt)
@@ -21,11 +21,29 @@ class Scramble_by_advancing_chars
   end
 
   def do_your_thing(contents_from_scram)
-    plain_chars = contents_from_scram
+    plain_chars = contents_from_scram.chars
     scrambled_chars = plain_chars.map do |char|
       (char.ord + @steps).chr
     end
     contents_from_scram = scrambled_chars.join
+    puts contents_from_scram
+  end
+
+end
+
+class Unscramble_by_advancing_chars
+
+  def initialize(steps)
+    @steps = steps
+  end
+
+  def do_your_thing(contents_from_scram)
+    scrambled_chars = contents_from_scram.chars
+    plain_chars = scrambled_chars.map do |char|
+      (char.ord - @steps).chr
+    end
+    contents_from_scram = plain_chars.join
+    puts contents_from_scram
   end
 
 end
@@ -33,7 +51,14 @@ end
 class Reverse
 
   def do_your_thing(contents_from_scram)
-    contents_from_scram.reverse
+    puts contents_from_scram.reverse
   end
-      
+
 end
+
+diary1 = ScrambledDiary.new("This is a load of content.")
+diary1.rejigger(Reverse.new)
+diary2 = ScrambledDiary.new("This is a load of content.")
+diary2.rejigger(Scramble_by_advancing_chars.new(10))
+diary3 = ScrambledDiary.new("^rs}*s}*k*vykn*yp*myx~ox~8")
+diary3.rejigger(Unscramble_by_advancing_chars.new(10))
